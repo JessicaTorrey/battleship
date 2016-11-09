d592b17d53932786ab3cf9f47d7cf88cc64e4454
mmazumder <mmazumder@newroads.org>
2016-11-09 12:07:28 -0500

    battleship added board

--- Battleship.java
+++ Battleship.java
@@ -1,7 +1,45 @@
 public class Battleship{
-  
   public static void main(String[] args){
     
-    System.out.print("Battleship");
+    char[][] ships = {
+                      {'x', ' ', ' ', ' ', ' '},
+                       {' ', ' ', ' ', ' ', ' '},
+                      {' ', ' ', ' ', ' ', ' '},
+                      {' ', ' ', ' ', ' ', ' '},
+                     {' ', ' ', ' ', ' ', ' '},
+                     {' ', ' ', ' ', ' ', ' '},
+                      {' ', ' ', ' ', ' ', ' '},
+                     {' ', ' ', ' ', ' ', ' '},
+                     {' ', ' ', ' ', ' ', ' '},
+                      {' ', ' ', 'x', ' ', ' '}
+                                                                                                                                                      
+      
+      
+      
+                    };
+    System.out.println("Battleship");
+     System.out.println("_________________________________________");
+    System.out.println("|  " + ships[0][0] + "    |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
+      System.out.println("|       |       |       |       |       |");
+    System.out.println("_________________________________________");
   }
+  
+  
 }
