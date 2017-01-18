ff2740cc768554d2837cfd5235d98a489fa593c9
abain <abain@newroads.org>
2017-01-18 12:38:11 -0500

    ship.java

---
+++ Ship.java
@@ -0,0 +1,20 @@
+public class Ship{
+  private int x1;
+  private int y1;
+  private int x2;
+  private int y2;
+ 
+}
+public Ship(){
+  
+}
+public Ship(int newx1, newy1){
+  x1 = newx1;
+  y1 = newy1;
+}
+public int getx1(){
+  return x1;
+}
+public void setx1(int newx1){
+  x1 = newx1
+}
